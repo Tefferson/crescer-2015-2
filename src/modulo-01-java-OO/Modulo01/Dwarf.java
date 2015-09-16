fb17afafaa2b0dde99f91c655984b203e50f7cfc
Tefferson <teffersonguterres@gmail.com>
2015-09-16 18:58:38 -0300

    style(correções): conflitos corrigidos

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 /**
  * Escreva a descrição da classe Dwarf aqui.
  * 
@@ -21,16 +19,3 @@ public class Dwarf
         return this.vida;
     }
 }
-=======
-public class Dwarf {
-    private int vida = 115;
-    
-    public void receberFlechada() {
-        this.vida -= 10;
-    }
-    
-    public int getVida() {
-        return this.vida;
-    }
-}
->>>>>>> 365f58ceeb9aa13772ec7cca7564d807fa7efa6e
--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -5,13 +5,7 @@ public class Elfo {
 
     private String nome;
     private int flechas, experiencia;
-
-    /* Type initializer
-     * Executa antes de cada construtor
-    {
-    flechas = 42;
-    }
-     */
+   
     public Elfo(String nome, int flechas) {
         this.nome = nome;
         this.flechas = flechas;
@@ -28,39 +22,12 @@ public class Elfo {
         this(nome, 42);
     }
 
-<<<<<<< HEAD
     public void atirarFlecha(Dwarf alvo) {
         flechas--;
         experiencia++;
         alvo.receberFlechada();
-=======
-    /* PascalCase (C#, VB.NET)
-     *      public void AtirarFlechaDeFogo
-     * camelCase (Java, JavaScript)
-     *      public void atirarFlechaDeFogo
-    */
-   
-    public void atirarFlecha(Dwarf dwarf) {
-        flechas--;
-        experiencia++;
-        dwarf.receberFlechada();
->>>>>>> 365f58ceeb9aa13772ec7cca7564d807fa7efa6e
-        //experiencia += 1;
-        //experiencia = experiencia + 1;
     }
-
     
-    /*
-     * 
-     * public atirarFlechaRefactory(this.flechas, this.experiencia){
-     *     if(boolean acertar == true){
-     *         flechas--;
-     *         experiencia++;
-     *      }else{
-     *          flechas--;
-     *      }
-     *  }
-     */
     public void atirarFlechaRefactory(){
         boolean acertar = true;
         if (acertar) {
@@ -72,22 +39,9 @@ public class Elfo {
     public String getNome() {
         return this.nome;
     }
-<<<<<<< HEAD
-    /*
-    public void setNome(String novoNome){
-        this.nome = novoNome;
-    }
-    */
-=======
     
->>>>>>> 365f58ceeb9aa13772ec7cca7564d807fa7efa6e
     public int getFlechas() {
         return this.flechas;
     }
-    /*
-    public void setFlechas(int flechas){
-        if(flechas>this.flechas)
-            this.flechas = flechas;
-    }
-    */
+    
 }
