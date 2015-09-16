dbb81a32e2170f2052f8773aa0f5d69a7fdf9049
Tefferson <teffersonguterres@gmail.com>
2015-09-16 09:09:05 -0300

    Exercícíos resolvidos

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,32 @@
+
 public class Elfo {
+
     private String nome;
     private int flechas;
-    
+    private int experiencia;
+
     public Elfo(String n, int flech) {
+        inicializarElfo(n, flech);
+    }
+
+    public Elfo(String n) {
+        inicializarElfo(n, 42);
+    }
+
+    private void inicializarElfo(String n, int flech) {
         nome = n;
         flechas = flech;
+        experiencia = 0;
+    }
+
+    public void atirarFlechaRefactory() {
+        if(acertarFlecha() == true) {
+            experiencia++;
+        }
+        flechas--;
+    }
+
+    private boolean acertarFlecha() {
+        return (Math.random() * 100 % 2) == 0;
     }
 }
