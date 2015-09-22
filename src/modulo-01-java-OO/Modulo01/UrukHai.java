d3cb1cbb4ee6be30b2b826717eb233b56a9f89b6
Tefferson <teffersonguterres@gmail.com>
2015-09-22 13:36:30 -0300

    refactor(Snaga): recebee sempre 10 de dano do dwarf

--- src/modulo-01-java-OO/Modulo01/UrukHai.java
+++ src/modulo-01-java-OO/Modulo01/UrukHai.java
@@ -5,4 +5,8 @@ public class UrukHai extends Orc
         this.vida = 150;
         this.adicionarItem(new Item("Espada", 1));
     }
+
+    public void receberFlechada(Dwarf dwarf) {
+        gerarDano(5);
+    }
 }
