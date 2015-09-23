996c75336acde8bd2d5839aad375dd06d9ed8c7b
Tefferson <teffersonguterres@gmail.com>
2015-09-23 13:30:54 -0300

    refactor(UrukHai): adicionar escudo no construtor

--- src/modulo-01-java-OO/Modulo01/UrukHai.java
+++ src/modulo-01-java-OO/Modulo01/UrukHai.java
@@ -4,5 +4,6 @@ public class UrukHai extends Orc
         super(nome);
         this.vida = 150;
         this.adicionarItem(new Item("Espada", 1));
+        this.adicionarItem(new Item("Escudo Uruk-Hai", 1));
     }
 }
