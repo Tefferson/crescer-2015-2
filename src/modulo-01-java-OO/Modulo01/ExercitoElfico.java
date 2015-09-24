51225a333c8e6e625b1a80c615feb085217110d6
Tefferson <teffersonguterres@gmail.com>
2015-09-24 02:42:28 -0300

    feat(ExercitoElfico): buscar e agrupar elfos pelo status

--- src/modulo-01-java-OO/Modulo01/ExercitoElfico.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElfico.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 /**
  * Write a description of class ExercitoElfico here.
  * 
@@ -8,8 +8,10 @@ import java.util.HashMap;
 public class ExercitoElfico
 {
     private HashMap<String, Elfo> exercito;
+    private HashMap<Status, HashMap<String, Elfo>> exercitoAgrupado;
     public ExercitoElfico(){
         exercito = new HashMap<>();
+        exercitoAgrupado = new HashMap<>();
     }
 
     public void alistarElfo(ElfoNoturno elfo){
@@ -26,5 +28,27 @@ public class ExercitoElfico
 
     public Elfo getElfo(String nome){
         return exercito.get(nome);
+    }
+
+    public void agruparPorStatus(){
+        this.exercitoAgrupado.clear();
+        for(Map.Entry<String, Elfo> entry : exercito.entrySet()){
+            Elfo elfo = entry.getValue();
+            Status status = elfo.getStatus();
+            HashMap<String, Elfo> innerMap = exercitoAgrupado.get(status);
+            if(innerMap==null){
+                innerMap = new HashMap<>();
+                exercitoAgrupado.put(status, innerMap);
+            }
+            innerMap.put(elfo.getNome(), elfo);
+        }
+    }
+
+    public ArrayList<Elfo> buscar(Status status){        
+        HashMap<String, Elfo> buscado = exercitoAgrupado.get(status);
+        if(buscado == null){
+            return null;
+        }
+        return new ArrayList<Elfo>(buscado.values());
     }
 }
--- src/modulo-01-java-OO/Modulo01/ExercitoElficoTest.java
+++ src/modulo-01-java-OO/Modulo01/ExercitoElficoTest.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -17,7 +17,46 @@ public class ExercitoElficoTest
         ExercitoElfico exercito = new ExercitoElfico();
         ElfoVerde esperado = new ElfoVerde("elfo");
         exercito.alistarElfo(esperado);
-                
+
         assertEquals(esperado, exercito.getElfo("elfo"));
     }
+
+    @Test
+    public void agrupaExercitoPorStatus(){    
+        ExercitoElfico exercito = new ExercitoElfico();
+        ElfoVerde elfo1 = new ElfoVerde("elfo1");
+        ElfoVerde elfo2 = new ElfoVerde("elfo2");
+        ElfoVerde elfo3 = new ElfoVerde("elfo3");
+        ElfoVerde elfo4 = new ElfoVerde("elfo4");
+        ElfoVerde elfo5 = new ElfoVerde("elfo5");
+        ElfoVerde elfo6 = new ElfoVerde("elfo6");
+        ElfoVerde elfo7 = new ElfoVerde("elfo7");
+        ElfoVerde elfo8 = new ElfoVerde("elfo8");
+        ElfoVerde elfo9 = new ElfoVerde("elfo9");
+        ElfoVerde elfox = new ElfoVerde("elfox");
+        exercito.alistarElfo(elfo1);
+        exercito.alistarElfo(elfo2);
+        exercito.alistarElfo(elfo3);
+        exercito.alistarElfo(elfo4);
+        exercito.alistarElfo(elfo5);
+        exercito.alistarElfo(elfo6);
+        exercito.alistarElfo(elfo7);
+        exercito.alistarElfo(elfo8);
+        exercito.alistarElfo(elfo9);
+        exercito.alistarElfo(elfox);  
+        Orc orc = new UrukHai();
+        elfo3.receberDano(orc);
+        for(int i=0;i<90;i++){        
+            elfo6.receberDano(orc);
+        }
+
+        exercito.agruparPorStatus();
+        
+        List feridos = exercito.buscar(Status.FERIDO);
+        List mortos = exercito.buscar(Status.MORTO);
+        
+        assertEquals(elfo3, feridos.get(0));
+        assertEquals(elfo6, mortos.get(0));
+    }
+
 }
