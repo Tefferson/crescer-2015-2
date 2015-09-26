8b9ed0a7a7023f1869674742b42318017449cc9c
Tefferson <teffersonguterres@gmail.com>
2015-09-26 15:46:05 -0300

    refactor(EstrategiaVerde): corrigir diferenças

--- src/modulo-01-java-OO/Modulo01/EstrategiaVerde.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaVerde.java
@@ -5,19 +5,12 @@ public class EstrategiaVerde implements EstrategiaDeAtaque {
         exercito.agruparPorStatus();
         atacantes = new ArrayList<>();
         ArrayList<Elfo> atacarao = exercito.buscar(Status.VIVO);
-        double intencoesDeAtaque = atacantes.size() * dwarves.size();
-        int limiteDeAtaqueNoturno = (int)(intencoesDeAtaque * .3);
-        int ataques = 0;
         for(Elfo elfo : atacantes){
             if(elfo instanceof ElfoVerde){
                 atacantes.add(elfo);
                 atacarao.remove(elfo);
                 for(Dwarf dwarf : dwarves){
-                    if(!(elfo instanceof ElfoNoturno && ataques>limiteDeAtaqueNoturno)){
-                        elfo.atirarFlecha(dwarf);
-                    }else{                    
-                        ataques++;                
-                    }
+                    elfo.atirarFlecha(dwarf);
                 }
             }
         }
@@ -27,11 +20,7 @@ public class EstrategiaVerde implements EstrategiaDeAtaque {
                 atacantes.add(elfo);
                 atacarao.remove(elfo);
                 for(Dwarf dwarf : dwarves){
-                    if(!(elfo instanceof ElfoNoturno && ataques>limiteDeAtaqueNoturno)){
-                        elfo.atirarFlecha(dwarf);
-                    }else{                    
-                        ataques++;                
-                    }
+                    elfo.atirarFlecha(dwarf);
                 }
             }
         }
