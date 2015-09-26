cc8030852faf7ce4af191b5576b61978316a8400
Tefferson <teffersonguterres@gmail.com>
2015-09-26 17:58:35 -0300

    feat(estrategiaNormal): atacar todos dwarves com todos elfos vivos

--- src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
@@ -1,19 +1,15 @@
 import java.util.*;
-public class EstrategiaNormal implements EstrategiaDeAtaque{
-    private ArrayList<Elfo> atacantes;
+public class EstrategiaNormal implements EstrategiaDeAtaque
+{
+    private ArrayList<Elfo> atacantes = new ArrayList<Elfo>();
     public void atacar(ExercitoElfico exercito, ArrayList<Dwarf> dwarves){
+        atacantes.clear();
         exercito.agruparPorStatus();
-        atacantes = exercito.buscar(Status.VIVO);
-        double intencoesDeAtaque = atacantes.size() * dwarves.size();
-        int limiteDeAtaqueNoturno = (int)(intencoesDeAtaque * .3);
-        int ataques = 0;
-        for(Elfo elfo : atacantes){
+        ArrayList<Elfo> vivos = exercito.buscar(Status.VIVO);
+        for(Elfo elfo : vivos){
+            atacantes.add(elfo);
             for(Dwarf dwarf : dwarves){
-                if(!(elfo instanceof ElfoNoturno && ataques>limiteDeAtaqueNoturno)){
-                    elfo.atirarFlecha(dwarf);
-                }else{                    
-                    ataques++;                
-                }
+                elfo.atirarFlecha(dwarf);
             }
         }
     }
--- src/modulo-01-java-OO/Modulo01/EstrategiaNormalTest.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormalTest.java
@@ -1,19 +1,26 @@
+
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.*;;
+import java.util.*;
 
+/**
+ * The test class EstrategiaNormalTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
 public class EstrategiaNormalTest
 {
     @Test
-    public void exercitoAtacaHordaDeDwarves(){
-        ExercitoElfico exercito = new ExercitoElfico();        
+    public void exercitoElficoAtacaHordaDeDwarves(){
+        ExercitoElfico exercito = new ExercitoElfico();      
         ArrayList<Dwarf> hordaDeDwarves = new ArrayList<>();
         int qtdElfosNoturnos = 15;
         int qtdElfosVerdes = 15;
         int qtdDwarves = 45;
-        int maximoDeElfosNoturnosQueDeveriamAtacar = 15;
+        int esperado = qtdElfosVerdes+qtdElfosNoturnos;
 
         for(int i=0;i<qtdElfosVerdes;i++){
             exercito.alistar(new ElfoVerde("elfoVerde"+i, 1000));
@@ -28,88 +35,38 @@ public class EstrategiaNormalTest
 
         exercito.atacar(hordaDeDwarves);
 
-        exercito.agruparPorStatus();
-        int elfosNoturnosQueAtacaram = exercito.buscar(Status.FERIDO).size();
-        assertEquals(maximoDeElfosNoturnosQueDeveriamAtacar, elfosNoturnosQueAtacaram);
+        int elfosQueAtacaram = exercito.getOrdemDoUltimoAtaque().size();
+        assertEquals(esperado, elfosQueAtacaram);
     }
 
     @Test
-    public void estrategia1Elfo2Dwarves() {
-        // Arrange
-        ExercitoElfico exercito = new ExercitoElfico();
-        Dwarf d1 = new Dwarf("dwarf1");
-        Dwarf d2 = new Dwarf("dwarf2");
-        ArrayList<Dwarf> alvos = new ArrayList<Dwarf>(
-                Arrays.asList(d1, d2)
-            );
-        ElfoVerde soldado = new ElfoVerde("Green Elf 1");
+    public void dwarvesSofrem4AtaquesCada(){
+        ExercitoElfico exercito = new ExercitoElfico();      
+        ArrayList<Dwarf> hordaDeDwarves = new ArrayList<>();
+        int qtdElfosNoturnos = 3;
+        int qtdElfosVerdes = 1;
+        int qtdDwarves = 45;
+        int totalElfos = qtdElfosVerdes+qtdElfosNoturnos;
+        int vidaEsperada = Math.abs(110 - totalElfos*10)*qtdDwarves;
 
-        double vidaEsperada = 100;
-        // Act
-        exercito.alistar(soldado);
-        exercito.atacar(alvos);
-        // Assert
-        assertEquals(new ArrayList<Elfo>(
-                Arrays.asList(soldado)
-            ), exercito.getOrdemDoUltimoAtaque());
+        for(int i=0;i<qtdElfosVerdes;i++){
+            exercito.alistar(new ElfoVerde("elfoVerde"+i, 1000));
+        }
+        for(int i=0;i<qtdElfosNoturnos;i++){
+            exercito.alistar(new ElfoNoturno("elfoNoturno"+i, 1000));
+        }
 
-        assertEquals(vidaEsperada, d1.getVida(), .0);
-        assertEquals(vidaEsperada, d2.getVida(), .0);
-    }
+        for(int i=0;i<qtdDwarves;i++){
+            hordaDeDwarves.add(new Dwarf("dwarf"+i));
+        }
 
-    @Test
-    public void estrategia3Elfos2Dwarves() {
-        // Arrange
-        Dwarf d1 = new Dwarf("d1");
-        Dwarf d2 = new Dwarf("d2");
-        ArrayList<Dwarf> alvos = new ArrayList<Dwarf>(
-                Arrays.asList(d1, d2)
-            );
-        Elfo soldado1 = new ElfoVerde("Green Elf 1");
-        Elfo soldado2 = new ElfoNoturno("Green Elf 2");
-        Elfo soldado3 = new ElfoVerde("Green Elf 3");
-        ExercitoElfico exercito = new ExercitoElfico();
-        double vidaEsperada = 80;
-        // Act
-        exercito.alistar(soldado1);
-        exercito.alistar(soldado2);
-        exercito.alistar(soldado3);
-        exercito.atacar(alvos);
-        // Assert
-        assertEquals(new ArrayList<Elfo>(
-                Arrays.asList(soldado3, soldado2, soldado1)
-            ), exercito.getOrdemDoUltimoAtaque());
+        exercito.atacar(hordaDeDwarves);
 
-        assertEquals(vidaEsperada, d1.getVida(), .0);
-        assertEquals(vidaEsperada, d2.getVida(), .0);
-    }
+        int vidaTotalRemanescente = 0;
+        for(int i=0;i<qtdDwarves;i++){
+            vidaTotalRemanescente+=hordaDeDwarves.get(0).getVida();
+        }
 
-    @Test
-    public void estrategia3Elfos2Dwarves2OndasDeAtaque() {
-        // Arrange
-        Dwarf d1 = new Dwarf("d1");
-        Dwarf d2 = new Dwarf("d2");
-        ArrayList<Dwarf> alvos = new ArrayList<Dwarf>(
-                Arrays.asList(d1, d2)
-            );
-        Elfo soldado1 = new ElfoVerde("Green Elf 1");
-        Elfo soldado2 = new ElfoNoturno("Green Elf 2");
-        Elfo soldado3 = new ElfoVerde("Green Elf 3");
-        ArrayList<Elfo> esperado = new ArrayList<Elfo>(Arrays.asList(soldado3, soldado2, soldado1));
-        ExercitoElfico exercito = new ExercitoElfico();
-        double vidaEsperada = 60;
-        // Act
-        exercito.alistar(soldado1);
-        exercito.alistar(soldado2);
-        exercito.alistar(soldado3);
-        exercito.atacar(alvos);
-        exercito.atacar(alvos);
-        ArrayList<Elfo> historicoAtaque = exercito.getOrdemDoUltimoAtaque();
-        // Assert
-        assertTrue(historicoAtaque.contains(soldado1));            
-        assertTrue(historicoAtaque.contains(soldado3));
-
-        assertEquals(vidaEsperada, d1.getVida(), .0);
-        assertEquals(vidaEsperada, d2.getVida(), .0);
+        assertEquals(vidaEsperada, vidaTotalRemanescente);
     }
 }
