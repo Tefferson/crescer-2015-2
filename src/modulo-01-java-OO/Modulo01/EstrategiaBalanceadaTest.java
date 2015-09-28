d11740a916c4661f99109ea956ac4f852644ffdb
Tefferson <teffersonguterres@gmail.com>
2015-09-28 13:14:42 -0300

    test(EstrategiaBalanceadaTest): testar ultima ordem de ataque

--- src/modulo-01-java-OO/Modulo01/EstrategiaBalanceadaTest.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaBalanceadaTest.java
@@ -39,4 +39,42 @@ public class EstrategiaBalanceadaTest
         int elfosQueAtacaram = exercito.getOrdemDoUltimoAtaque().size();
         assertEquals(esperado, elfosQueAtacaram);
     }
+
+    @Test
+    public void confereUltimaOrdemBalanceada(){
+        ExercitoElfico exercito = new ExercitoElfico();      
+        exercito.trocarEstrategia(new EstrategiaBalanceada());
+        ArrayList<Dwarf> hordaDeDwarves = new ArrayList<>();
+        int qtdElfosNoturnos = 15;
+        int qtdElfosVerdes = 150;
+        int qtdDwarves = 45;
+        int menorQtd = qtdElfosNoturnos<qtdElfosVerdes?qtdElfosNoturnos:qtdElfosVerdes;
+        int esperado = 2 * menorQtd;
+
+        for(int i=0;i<qtdElfosNoturnos;i++){
+            exercito.alistar(new ElfoNoturno("elfoNoturno"+i, 1000));
+        }
+        for(int i=0;i<qtdElfosVerdes;i++){
+            exercito.alistar(new ElfoVerde("elfoVerde"+i, 1000));
+        }
+
+        for(int i=0;i<qtdDwarves;i++){
+            hordaDeDwarves.add(new Dwarf("dwarf"+i));
+        }
+
+        exercito.atacar(hordaDeDwarves);
+
+        int elfosVerdesAtacantes=0, elfosNoturnosAtacantes=0;
+        ArrayList<Elfo> atacantes = exercito.getOrdemDoUltimoAtaque();
+        for(Elfo elfo : atacantes){
+            if(elfo instanceof ElfoVerde){
+                elfosVerdesAtacantes++;
+            }else if(elfo instanceof ElfoNoturno){
+                elfosNoturnosAtacantes++;
+            }
+        }
+
+        assertEquals(menorQtd, elfosNoturnosAtacantes);
+        assertEquals(elfosVerdesAtacantes, elfosNoturnosAtacantes);
+    }
 }
