6ec196cbdedb808ceca20431e4beef46f52bd85f
Tefferson <teffersonguterres@gmail.com>
2015-10-20 17:03:55 -0200

    feat(Elfo): implementar classe Elfo de exemplo

---
+++ src/modulo-03-web/dia-03/Elfo.js
@@ -0,0 +1,10 @@
+function Elfo(nome, flechas){
+  this.nome = nome;
+  this.flechas = flechas || 42;
+
+  Object.defineProperty(this,'flechas',{writable:false});
+
+  Elfo.prototype.atirarFlecha = function (dwarf) {
+    console.log('Atirando flecha...');
+  };
+};
