83ff735b1793db6f108059a6143a1b44abddd324
Tefferson <teffersonguterres@gmail.com>
2015-10-20 00:25:25 -0200

    style(lambda): substituir por lambda, ligeiramente citado na última aula

--- src/modulo-03-web/dia-02/exercicios-colecoes-js.js
+++ src/modulo-03-web/dia-02/exercicios-colecoes-js.js
@@ -1,11 +1,11 @@
 function ordenaPorNacionais(clubes){
 	ordenarPorTitulo(0,clubes);
 };
-function ordenaPorMundiais(clubes){
-	ordenarPorTitulo(2,clubes);
-};
 function ordenaPorContinentais(clubes){
 	ordenarPorTitulo(1,clubes);
+};
+function ordenaPorMundiais(clubes){
+	ordenarPorTitulo(2,clubes);
 };
 
 function somarPorNacionais(clubes){
@@ -19,23 +19,15 @@ function somarPorMundiais(clubes){
 };
 
 function ordenarPorTitulo(tipoTitulo, clubes){
-	clubes.sort(
-		function(a,b){
-			return a.titulos[tipoTitulo].qtd < b.titulos[tipoTitulo].qtd;
-		}
-	);
+	clubes.sort((a,b)=>a.titulos[tipoTitulo].qtd < b.titulos[tipoTitulo].qtd);
 };
 
 function somarTitulos(tipoTitulo, clubes){
 	var soma=0;
-	clubes.forEach(function(clube){
-		soma+=clube.titulos[tipoTitulo].qtd;
-	});
+	clubes.forEach(e=>soma+=e.titulos[tipoTitulo].qtd);
 	return soma;
 };
 
 function apenasOsMelhores(clubes){
-	return clubes.filter(function(clube){
-		return clube.titulos[0].qtd>18;
-	});
+	return clubes.filter(e=>e.titulos[0].qtd>18);
 };
