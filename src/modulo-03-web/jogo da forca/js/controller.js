a4cbfb949bda7c52577708d7693cc755f3b43887
Tefferson <teffersonguterres@gmail.com>
2015-10-23 18:54:51 -0200

    feat(initGame): desabilitar e habiltar botões

--- src/modulo-03-web/jogo da forca/js/controller.js
+++ src/modulo-03-web/jogo da forca/js/controller.js
@@ -3,28 +3,33 @@ function Controller(options){
 };
 
 Controller.prototype.initGame = function(){
+  $('.botao').prop('disabled', true);
   this.palavra='PALAVRA';
   this.jogo = new Jogo({palavra:this.palavra,maxErros:2, encerrarPartida:this.encerrarPartida});
   this.jogo.init();
   $('.palavra')[0].innerHTML=this.jogo.getPalavra();
+  $('.botao').prop('disabled', false);
 }
 
 Controller.prototype.init = function () {
   this.initGame();
-  $('.botao').click(function(e){this.verificarCompletude(e.toElement.value);}.bind(this));
+  $('.botao').click(function(e){this.verificarCompletude(e.toElement);}.bind(this));
 };
 
-Controller.prototype.verificarCompletude = function(letra){
-  this.jogo.chutarLetra(letra);
+Controller.prototype.verificarCompletude = function(elem){
+  this.jogo.chutarLetra(elem.value);
+  elem.disabled=true;
   if(this.jogo.estado==='derrota'){
     this.initGame();
   }else if(this.jogo.estado==='vitoria'){
-    alert('vitória!!!');this.palavra='PALAVRA';
-    this.jogo = new Jogo({palavra:this.palavra,maxErros:2, encerrarPartida:this.encerrarPartida});
-    this.jogo.init();
-    $('.palavra')[0].innerHTML=this.jogo.getPalavra();
+    this.postVitoria();
+    this.initGame();
   }
   $('.palavra')[0].innerHTML=this.jogo.getPalavra();
+}
+
+Controller.prototype.postVitoria = function(){
+  alert('vitoria!!!');
 }
 
 var controller = new Controller();
