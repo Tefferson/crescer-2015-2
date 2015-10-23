8eefe226a9aa33caab21591454631b37fe2277ca
Tefferson <teffersonguterres@gmail.com>
2015-10-23 02:02:37 -0200

    feat(douchebag): implementar exercício 3

--- src/modulo-03-web/dia-05/jQuery-ajax/js/consulta-ecletica.js
+++ src/modulo-03-web/dia-05/jQuery-ajax/js/consulta-ecletica.js
@@ -1,12 +1,20 @@
 $('body:eq(0)').css({'text-align':'center'});
 $('ul:eq(0)').css({'list-style':'none'});
 $('#submit').click(consultarArtista);
+$.ajaxSetup({
+  error: function(xhr, status, error) {
+    alert('Caro usuário, devido a um erro '+xhr.status+', não foi possível pesquisar por '+$('#artista').val());
+  }
+});
 
 function consultarArtista(){
   var nomeAConsultar = $('#artista').val().split(" ").join("+");
   $('ul:eq(0)').empty();
   nomeAConsultar.length &&
   $.get('https://api.spotify.com/v1/search?q='+nomeAConsultar+'&type=artist').done(function(data) {
+    if(nomeAConsultar==='Justin+Bieber'){
+      data.artists.items[0].id=Math.random()<0.8?'douchebag':'1uNFoZAHBGtllmzznpCI3s';
+    }
     data.artists.items.length &&
     consultarAlbuns(data.artists.items[0].id);
   });
