29425aed142f89eebe584cc6289ebd7ec57ac4bf
Tefferson <teffersonguterres@gmail.com>
2015-10-29 00:39:23 -0200

    feat(Teclado): implementar classe teclado

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Teclado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public class Teclado
+    {
+        public Teclado() { }
+
+        public Int32? LerInt()
+        {
+            string digitado = Console.ReadLine();
+            int num;
+            if (digitado != "" && Int32.TryParse(digitado, out num))
+            {
+                return num;
+            }
+            return null;
+        }
+
+        public Double? LerDouble()
+        {
+            string digitado = Console.ReadLine();
+            double num;
+            if (digitado != "" && Double.TryParse(digitado, out num))
+            {
+                return num;
+            }
+            return null;
+        }
+
+        public string LerString()
+        {
+            string digitado = Console.ReadLine();
+            return digitado != "" ? digitado : null;
+        }
+
+        public string LerLinha()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
