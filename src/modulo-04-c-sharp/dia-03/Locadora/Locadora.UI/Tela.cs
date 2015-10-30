ca00becbe3d830ecb840dd9ec26dfdcc0fbf1081
Tefferson <teffersonguterres@gmail.com>
2015-10-30 20:23:44 -0200

    feat(mensagens): exibir mensagens antes de retornar ao menu de opções

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.UI/Tela.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.UI/Tela.cs
@@ -9,14 +9,13 @@ namespace Locadora.UI
 {
     class Tela
     {
-        const string CONTINUAR = "Por favor, pressione ENTER se quiser continuar...";
-        const string INFORMAR_NOME = "Por favor, informe um nome:";
-
         internal void start()
         {
             while (UpdateTela()) ;
         }
 
+        const string CONTINUAR = "Por favor, pressione ENTER se quiser continuar...";
+        const string INFORMAR_NOME = "Por favor, informe um nome:";
         const string INFORMAR_PRECO = "Por favor, informe um preço:";
         const string INFORMAR_OPCAO = "Por favor, informe uma opção:";
         const string INFORMAR_CATEGORIA = "Por favor, informe uma categoria:";
@@ -28,6 +27,18 @@ namespace Locadora.UI
         const string PESQUISA_CONCLUIDA = "Pesquisa concluída. Por favor, pressione ENTER para ir ao menu...";
         const string PESQUISA_EDITAR_FALHA = "Desculpe, o jogo desejado não existe.";
         const string RELATORIO_GERADO = "Relatório gerado. Por favor, pressione ENTER para ir ao menu...";
+        const string JOGO_DEVOLVIDO = "Jogo devolvido, pressione ENTER para ir ao menu...";
+        const string JOGO_ESTAVA_DISPONIVEL = "Jogo já estava disponível. Por favor, pressione ENTER para ir ao menu...";
+        const string LOCAR_SUCESSO = "Jogo locado com sucesso!!!";
+        const string LOCAR_FALHA = "Desculpe, não foi possível locar o jogo...";
+        const string CLIENTE_CADASTRADO_FALHA = "Desculpe, não foi possível cadastrar o cliente...";
+        const string CLIENTE_CADASTRADO = "Cliente cadastrado com sucesso!!!";
+        const string EDITAR_JOGO_FALHA = "Desculpe, não foi possível editar o jogo...";
+        const string JOGO_EDITADO = "Jogo editado com sucesso!!!";
+        const string VOLTANDO_PARA_MENU = "Voltando para o menu, por favor pressione ENTER...";
+        const string CADASTRO_JOGO_SUCESSO = "Jogo cadstrado com sucesso!!!";
+        const string CADASTRO_JOGO_FALHA = "Desculpe, não foi possível cadastrar o jogo...";
+        const string TCHAU = "Tchau!!!";
         const int PESQUISAR_JOGO_POR_NOME = 1;
         const int LOCAR = 2;
         const int DEVOLVER = 3;
@@ -42,6 +53,11 @@ namespace Locadora.UI
         Teclado teclado = new Teclado();
         BaseDeDados dados;
         Relatorio relatorio;
+        private string[] OPCOES_DO_MENU = {
+                "1-Pesquisar jogo por nome", "2-Alugar jogo", "3-Devolver jogo",
+                "4-Cadastrar cliente", "5-Cadastrar jogo", "6-Editar jogo",
+                "7-Gerar relatório TXT", "0-Sair", Environment.NewLine};
+
         public Tela()
         {
             dados = new BaseDeDados(caminho);
@@ -86,10 +102,10 @@ namespace Locadora.UI
                 if (!dados.JogoIsDisponivel(idJogo))
                 {
                     dados.SetJogoDisponivel(idJogo, true);
+                    return VoltarParaMenuComMensagem(JOGO_DEVOLVIDO);
                 }
             }
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(JOGO_ESTAVA_DISPONIVEL);
         }
 
         private bool LocarJogo()
@@ -105,10 +121,10 @@ namespace Locadora.UI
                 {
                     dados.Cadastrar(new Locacao(idCliente, idJogo));
                     dados.SetJogoDisponivel(idJogo, false);
+                    return VoltarParaMenuComMensagem(LOCAR_SUCESSO);
                 }
             }
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(LOCAR_FALHA);
         }
 
         private bool CadastrarCliente()
@@ -117,18 +133,15 @@ namespace Locadora.UI
             if (teclado.LerNome(INFORMAR_NOME, INFORMAR_NOME, out nome))
             {
                 dados.Cadastrar(new Cliente(nome));
+                return VoltarParaMenuComMensagem(CLIENTE_CADASTRADO);
             }
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(CLIENTE_CADASTRADO_FALHA);
         }
 
         private bool GerarRelatorio()
         {
             relatorio.ExportarRelatorioEmTxt(dados);
-            EscreverMensagens(true, RELATORIO_GERADO);
-            teclado.LerLinha();
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(RELATORIO_GERADO);
         }
 
         private bool PesquisarJogoPorNome()
@@ -140,11 +153,9 @@ namespace Locadora.UI
             if (!String.IsNullOrEmpty(nome))
             {
                 PesquisarJogoPorNome(nome);
-                EscreverMensagens(true, PESQUISA_CONCLUIDA);
-                teclado.LerLinha();
             }
-            current = Telas.Menu;
-            return true;
+
+            return VoltarParaMenuComMensagem(PESQUISA_CONCLUIDA);
         }
 
         private IList<Jogo> PesquisarJogoPorNome(string nome)
@@ -166,7 +177,7 @@ namespace Locadora.UI
 
         private bool Sair()
         {
-            EscreverMensagens(true, "Tchau!!!");
+            EscreverMensagens(true, TCHAU);
             teclado.LerLinha();
             return false;
         }
@@ -180,22 +191,17 @@ namespace Locadora.UI
 
             if (!teclado.LerNome(INFORMAR_NOME, DIGITAR_ENTER_PARA_VOLTAR, out nome))
             {
-                current = Telas.Menu;
-                return true;
+                return VoltarParaMenuComMensagem(VOLTANDO_PARA_MENU);
             }
             EscreverCabecalho();
             if (!SelecionarJogoParaEditar(PesquisarJogoPorNome(nome), out editar))
             {
-                current = Telas.Menu;
-                return true;
+                return VoltarParaMenuComMensagem(EDITAR_JOGO_FALHA);
             }
 
             if (editar == null)
             {
-                EscreverMensagens(false, DIGITAR_ENTER_PARA_VOLTAR);
-                teclado.LerLinha();
-                current = Telas.Menu;
-                return true;
+                return VoltarParaMenuComMensagem(DIGITAR_ENTER_PARA_VOLTAR);
             }
 
             if (teclado.LerNome(INFORMAR_NOME, DIGITAR_ENTER_PARA_VOLTAR, out nome))
@@ -210,13 +216,12 @@ namespace Locadora.UI
                         editar.Nome = nome;
                         editar.Preco = preco;
                         editar.Categoria = categoria.ToString().ToUpper();
-                        current = Telas.Menu;
                         dados.EditarJogo(editar);
+                        return VoltarParaMenuComMensagem(JOGO_EDITADO);
                     }
                 }
             }
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(EDITAR_JOGO_FALHA);
         }
 
         private bool SelecionarJogoParaEditar(IList<Jogo> jogos, out Jogo jogo)
@@ -257,19 +262,16 @@ namespace Locadora.UI
                     {
                         EscreverMensagens();
                         dados.Cadastrar(new Jogo(nome, preco, categoria.ToString().ToUpper()));
+                        return VoltarParaMenuComMensagem(CADASTRO_JOGO_SUCESSO);
                     }
                 }
             }
-            current = Telas.Menu;
-            return true;
+            return VoltarParaMenuComMensagem(CADASTRO_JOGO_FALHA);
         }
 
         private bool Menu()
         {
-            EscreverMensagens(true,
-                "1-Pesquisar jogo por nome", "2-Alugar jogo", "3-Devolver jogo",
-                "4-Cadastrar cliente", "5-Cadastrar jogo", "6-Editar jogo",
-                "7-Gerar relatório TXT", "0-Sair", Environment.NewLine);
+            EscreverMensagens(true, OPCOES_DO_MENU);
             EscreverMensagens(false, INFORMAR_OPCAO);
             int? op = teclado.LerInt();
             while (op == null)
@@ -350,6 +352,17 @@ namespace Locadora.UI
             Console.WriteLine(FormatTo80(current.ToString()));
             Console.WriteLine(Relatorio.IGUAIS);
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private bool VoltarParaMenuComMensagem(string mensagem)
+        {
+            if (!String.IsNullOrEmpty(mensagem))
+            {
+                Console.Write(FormatTo80(mensagem));
+                Console.ReadKey();
+            }
+            current = Telas.Menu;
+            return true;
         }
     }
 }
