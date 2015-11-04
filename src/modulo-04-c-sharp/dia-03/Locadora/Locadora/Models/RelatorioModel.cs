568d40666c7bfb95d3dc658cc6cef2b9ee454cc5
Tefferson <teffersonguterres@gmail.com>
2015-11-04 00:25:47 -0200

    feat(MVC): implementar padrão MVC.

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora/Models/RelatorioModel.cs
@@ -0,0 +1,16 @@
+﻿using Locadora.Dominio;
+
+namespace Locadora.Models
+{
+    public class RelatorioModel
+    {
+        private Relatorio relatorio;
+
+        public string Relatorio { get { return relatorio.GerarRelatorio(); } }
+
+        public RelatorioModel()
+        {
+            relatorio = new Relatorio();
+        }
+    }
+}
