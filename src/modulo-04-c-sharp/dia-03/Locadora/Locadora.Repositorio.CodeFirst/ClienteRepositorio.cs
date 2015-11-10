8ff4377000782c5823d35b8a47b343eb6c6962b8
Tefferson <teffersonguterres@gmail.com>
2015-11-10 00:12:04 -0200

    refactor(EF): implementar interface IClienteRepositorio usando EF

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/ClienteRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Locadora.Dominio.Repositorio;
+using System.Collections.Generic;
+using System.Linq;
+using Locadora.Dominio;
+
+namespace Locadora.Repositorio.EF
+{
+    class ClienteRepositorio : IClienteRepositorio
+    {
+        public IList<Cliente> BuscarPorNome(string nome)
+        {
+            using (var db = new BancoDeDados())
+            {
+                return db.Cliente.Where(cliente => cliente.Nome.Contains(nome)).ToList();
+            }
+        }
+    }
+}
