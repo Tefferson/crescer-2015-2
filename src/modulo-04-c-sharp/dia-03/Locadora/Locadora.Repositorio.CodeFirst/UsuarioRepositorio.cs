08da4aaa7de7cab2422182b4979212bade49d999
Tefferson <teffersonguterres@gmail.com>
2015-11-11 15:49:45 -0200

    refactor(UsuarioRepositorio): simplificar busca

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/UsuarioRepositorio.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/UsuarioRepositorio.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Locadora.Dominio;
+﻿using Locadora.Dominio;
 using Locadora.Dominio.Repositorio;
 using System.Linq;
 
@@ -11,30 +10,7 @@ namespace Locadora.Repositorio.EF
         {
             using (var db = new BancoDeDados())
             {
-                var usuariosEPermissoes = db.Usuario
-                    .Where(u => u.Email == email)
-                    .SelectMany(
-                    u => u.Permissoes.Select(p =>
-                       new
-                       {
-                           Usuario = u,
-                           Permissao = p
-                       }
-                        )
-                    ).GroupBy(u => u.Usuario.Email)
-                    .FirstOrDefault();
-
-                if (usuariosEPermissoes != null)
-                {
-                    var permissoes = usuariosEPermissoes.Select(m => m.Permissao).ToArray();
-
-                    var user = usuariosEPermissoes.FirstOrDefault().Usuario;
-                    return new Usuario(user.NomeCompleto, user.Email, user.Senha)
-                    {
-                        Permissoes = permissoes
-                    };
-                }
-                return null;
+                return db.Usuario.Include("Permissoes").FirstOrDefault(u => u.Email == email);
             }
         }
     }
