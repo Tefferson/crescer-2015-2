0064a73dc76920a0e30883a7cf042b22cd818d3f
Tefferson <teffersonguterres@gmail.com>
2015-11-12 17:36:38 -0200

    refactor(JogoMap): refatorar map do jogo

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/Mapping/JogoMap.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/Mapping/JogoMap.cs
@@ -14,9 +14,9 @@ namespace Locadora.Repositorio.EF.Mapping
             Property(p => p.Descricao).IsRequired().HasMaxLength(250);
             Property(p => p.Imagem).IsRequired().HasMaxLength(250);
             Property(p => p.Video).IsRequired().HasMaxLength(250);
+            Property(p => p.Disponivel).IsRequired();
             Property(p => p.Categoria).IsRequired().HasColumnName("IdCategoria");
-
-            HasOptional(o => o.ClienteLocacao).WithOptionalDependent().Map(m => m.MapKey("IdClienteLocacao"));
+            
             HasRequired(o => o.Selo).WithRequiredDependent().Map(m => m.MapKey("IdSelo"));
         }
 
