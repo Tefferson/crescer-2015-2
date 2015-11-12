ef61fa81fa4cd6f27a958098ec74d39b85a1b839
Tefferson <teffersonguterres@gmail.com>
2015-11-12 05:50:06 -0200

    feat(LocacaoModel): criar model para view de locação

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Web.MVC/Models/LocacaoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class LocacaoModel
+    {
+        [Required]
+        public int Id { get; set; }
+        public string NomeJogo { get; set; }
+        public DateTime DataPrevista { get; set; }
+        public decimal Valor { get; set; }
+        public string Imagem { get; set; }
+        [Required]
+        [DisplayName("Cliente")]
+        public string NomeCliente { get; set; }
+    }
+}
