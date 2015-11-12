e36ec766df2cd925565f6513e1b22b14075e5fc0
Tefferson <teffersonguterres@gmail.com>
2015-11-12 05:51:16 -0200

    feat(SeloRepositorio): criar e implementar interface do repositório de Selo

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Repositorio/ISeloRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Locadora.Dominio.Repositorio
+{
+    public interface ISeloRepositorio
+    {
+        Selo BuscarPorNome(string nome);
+        IList<string> BuscarTodosNomes();
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Repositorio.CodeFirst/SeloRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using Locadora.Dominio.Repositorio;
+using System.Collections.Generic;
+using System.Linq;
+using Locadora.Dominio;
+using System;
+
+namespace Locadora.Repositorio.EF
+{
+    public class SeloRepositorio : ISeloRepositorio
+    {
+        public IList<string> BuscarTodosNomes()
+        {
+            using (var db = new BancoDeDados())
+            {
+                return db.Selo.Select(s => s.Nome).ToList();
+            }
+        }
+
+        public Selo BuscarPorNome(string nome)
+        {
+            using (var db = new BancoDeDados())
+            {
+                var ss = db.Selo.FirstOrDefault(selo => selo.Nome == nome);
+                return ss;
+            }
+        }
+    }
+}
