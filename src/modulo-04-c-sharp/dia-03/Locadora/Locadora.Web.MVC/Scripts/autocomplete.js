12d7a0784159ddb16747de55a1a05a1a7afc79f2
Tefferson <teffersonguterres@gmail.com>
2015-11-12 05:48:35 -0200

    refactor(autocomplete): implementar autocomplete na tela de locação

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Web.MVC/Scripts/autocomplete.js
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Web.MVC/Scripts/autocomplete.js
@@ -1,17 +1,23 @@
 ﻿$(document).ready(function () {
-    $(".texto-busca").autocomplete({
+
+    var textoBusca = $(".texto-busca").autocomplete({
         source: '/Relatorio/Autocomplete',
         minLength: 2,
         select: function (event, ui) {
+            event.preventDefault();
             if (ui.item) {
                 if (ui.item.value === '*') {
+                    $('.texto-busca').val('');
                     $('.form-filtro').submit();
                 } else {
                     location.href = '/Jogo/Detalhes/' + ui.item.value;
                 }
             }
-        }
-    }).data("autocomplete")._renderItem = function (ul, item) {
+        },
+        focus: function (event, ui) { event.preventDefault(); $(".texto-busca").val(ui.item.label); }
+    });
+
+    textoBusca.data("autocomplete")._renderItem = function (ul, item) {
         var newItem = $("<li>").data("item.autocomplete", item);
         if (item.icon) {
             newItem.append("<img width='50' height=50 src=" + item.icon + " />")
@@ -22,12 +28,36 @@
         if (item.value === '*') newItem.addClass('item-ver-tudo');
         return newItem;
     };
-});
-$.ui.autocomplete.prototype._renderMenu = function (ul, items) {
-    var self = this;
-    $.each(items, function (index, item) {
-        if (index < 5)
-        { self._renderItem(ul, item); }
+
+    textoBusca.data("autocomplete")._renderMenu = function (ul, items) {
+        var self = this;
+        console.log(self);
+        $.each(items, function (index, item) {
+            if (index < 5)
+            { self._renderItem(ul, item); }
+        });
+        self._renderItem(ul, { label: 'Ver tudo', value: '*' });
+        $('.ui-menu').addClass("jogo-lista-autocomplete");
+    };
+
+    var clienteBusca = $('.cliente-busca').autocomplete({
+        source: '/Locacao/Autocomplete',
+        minLength: 2,
+        select: function (event, ui) {
+            event.preventDefault();
+            if (ui.item) {
+                location.href = '/Jogo/Detalhes/' + ui.item.value;
+            }
+        },
+        focus: function (event, ui) { event.preventDefault(); $(".cliente-busca").val(ui.item.label); }
     });
-    self._renderItem(ul, { label: 'Ver tudo', value: '*' });
-}
+
+    clienteBusca.data("autocomplete")._renderMenu = function (ul, items) {
+        var self = this;
+        $.each(items, function (index, item) {
+            if (index < 5)
+            { self._renderItem(ul, item); }
+        });
+        $('.ui-menu').addClass("cliente-lista-autocomplete");
+    };
+});
