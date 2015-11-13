4975799399603c83502a5be5296cf967aacff7fe
Tefferson <teffersonguterres@gmail.com>
2015-11-13 05:16:27 -0200

    style(views): estilizar e adicionar idlocadora em devolucaomodel

--- src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Web.MVC/Models/DevolucaoModel.cs
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Web.MVC/Models/DevolucaoModel.cs
@@ -4,6 +4,7 @@ namespace Locadora.Web.MVC.Models
 {
     public class DevolucaoModel
     {
+        public int IdLocacao { get; set; }
         public string NomeJogo { get; set; }
         public DateTime DataLocacao { get; set; }
         public decimal ValorFinal { get; set; }
