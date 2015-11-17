b246ae8a090e65ad1ce50355e78c91aad21b2764
Tefferson <teffersonguterres@gmail.com>
2015-11-17 17:52:00 -0200

    refactor(aula-02): organizar e implementar

---
+++ src/modulo-05-java/dia-02/application/br/com/cwi/application/screen/ListScreen.java
@@ -0,0 +1,38 @@
+package br.com.cwi.application.screen;
+
+import java.util.List;
+
+public class ListScreen implements IScreen {
+
+	private List<String> list;
+
+	public ListScreen(List<String> list) {
+		this.list = list;
+	}
+
+	@Override
+
+	public void show() {
+		System.out.println(options());
+	}
+
+	@Override
+	public void action() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String options() {
+		StringBuilder sb = new StringBuilder();
+
+		int size = list.size();
+		String newLine = System.getProperty("line.separator");
+
+		for (int i = 0; i < size; i++) {
+			sb.append(i + ": " + list.get(i) + newLine);
+		}
+		
+		return sb.toString();
+	}
+
+}
---
+++ src/modulo-05-java/dia-02/list/br/com/cwi/list/linked/LinkedList.java
@@ -0,0 +1,158 @@
+package br.com.cwi.list.linked;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.cwi.list.exception.EmptyListException;
+
+public class LinkedList<T> implements ILinkedList<T> {
+
+    protected Node<T> last, first;
+
+    @Override
+    public void addFirst(T value) {
+        Node<T> node = new Node<>(value, first);
+
+        if (isEmpty()) {
+            last = node;
+        }
+        first = node;
+    }
+
+    @Override
+    public T getFirst() {
+        if (isEmpty()) {
+            throw new EmptyListException();
+        }
+        return first.getValue();
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) {
+            throw new EmptyListException();
+        }
+        return last.getValue();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    @Override
+    public List<T> list() {
+        ArrayList<T> list = new ArrayList<>();
+
+        Node<T> actual = first;
+
+        while (actual != null) {
+            list.add(actual.getValue());
+            actual = actual.getNext();
+        }
+
+        return list;
+    }
+
+    @Override
+    public void addLast(T value) {
+        Node<T> newLast = new Node<>(value);
+
+        if (isEmpty()) {
+            first = newLast;
+        } else {
+            last.setNext(newLast);
+        }
+        last = newLast;
+    }
+
+    @Override
+    public void removeFirst() {
+        if (!isEmpty()) {
+            first = first.getNext();
+        }
+    }
+
+    @Override
+    public void add(int index, T value) {
+
+        if (index == 0 || isEmpty()) {
+            addFirst(value);
+        } else {
+            Node<T> actualNode = getNode(index);
+
+            if (actualNode != null) {
+                Node<T> newNode = new Node<>(value);
+
+                newNode.setNext(actualNode.getNext());
+
+                actualNode.setNext(newNode);
+            }
+        }
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index == 0 || isEmpty()) {
+            removeFirst();
+        } else {
+            Node<T> actualNode = getNode(index);
+
+            if (actualNode != null) {
+                actualNode.setNext(actualNode.getNext().getNext());
+            }
+        }
+    }
+
+    private Node<T> getNode(int index) {
+
+        Node<T> actualNode = first;
+        if (index > 0) {
+            for (int i = 1; i < index; i++) {
+                if (actualNode == null) {
+                    return null;
+                }
+                actualNode = actualNode.getNext();
+            }
+        }
+        return actualNode;
+    }
+
+    protected class Node<E> {
+
+        private E value;
+
+        private Node<E> next;
+
+        public Node(E value, Node<E> next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+
+    }
+}
