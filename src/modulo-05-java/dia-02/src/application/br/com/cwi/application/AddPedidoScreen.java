92e9ac4b487d07e5660e389eedb3e5f757595141
Tefferson <teffersonguterres@gmail.com>
2015-11-19 00:55:00 -0200

    feat(AddPedidoScreen): permitir a inclusão de cliente antes de adicionar pedido

--- src/modulo-05-java/dia-02/src/application/br/com/cwi/application/AddPedidoScreen.java
+++ src/modulo-05-java/dia-02/src/application/br/com/cwi/application/AddPedidoScreen.java
@@ -27,17 +27,7 @@ public class AddPedidoScreen implements IScreen {
 	@Override
 	public void action() {
 
-		List<Cliente> clientes = clienteDAO.listAll();
-
-		for (int i = 0; i < clientes.size(); i++) {
-			System.out.println(i + 1 + " - " + clientes.get(i).getNmCliente());
-		}
-
-		System.out.println("Informe o índice do cliente:");
-
-		Integer selectedIndex = scanner.nextInt();
-		scanner.nextLine();
-		Long idCliente = clientes.get(selectedIndex - 1).getId();
+		Long idCliente = selectIdCliente();
 
 		System.out.println("Informe a descrição do pedido:");
 
@@ -51,6 +41,63 @@ public class AddPedidoScreen implements IScreen {
 
 		System.out.println("Pedido incluído com sucesso!");
 		System.out.println();
+		System.out.println();
+	}
+
+	private Long selectIdCliente() {
+
+		Long idCliente = null;
+		boolean indexNotSelected = idCliente == null;
+
+		while (indexNotSelected) {
+			
+			idCliente = listClientesAndGetSelectedId();
+
+			if (idCliente == null) {
+				createCliente();
+			} else {
+				indexNotSelected = false;
+			}
+
+		}
+
+		return idCliente;
+	}
+
+	private Long listClientesAndGetSelectedId() {
+
+		List<Cliente> clientes = clienteDAO.listAll();
+
+		System.out.println("0 - Incluir cliente");
+		
+		for (int i = 0; i < clientes.size(); i++) {
+			System.out.println(i + 1 + " - " + clientes.get(i).getNmCliente());
+		}
+
+		System.out.println("Informe o índice do cliente:");
+
+		Integer selectedIndex = scanner.nextInt();
+		scanner.nextLine();
+
+		boolean selectedIndexIsValid = selectedIndex > 0;
+
+		return selectedIndexIsValid ? clientes.get(selectedIndex - 1).getId() : null;
+	}
+
+	private void createCliente() {
+
+		System.out.println("Informe o nome do cliente:");
+		String nmCliente = scanner.nextLine();
+
+		System.out.println("Informe o CPF do cliente:");
+		String nrCpf = scanner.nextLine();
+
+		Cliente cliente = new Cliente();
+		cliente.setNmCliente(nmCliente);
+		cliente.setNrCpf(nrCpf);
+
+		clienteDAO.insert(cliente);
+		System.out.println("Cliente incluído com sucesso!");
 		System.out.println();
 	}
 
