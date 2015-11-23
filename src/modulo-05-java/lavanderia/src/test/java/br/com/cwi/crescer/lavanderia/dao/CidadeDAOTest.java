b2c2eed2a26f05766a98143a86c7306097764a99
Tefferson <teffersonguterres@gmail.com>
2015-11-23 15:39:25 -0200

    test(Lavanderia): adicionar testes.

---
+++ src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/lavanderia/dao/CidadeDAOTest.java
@@ -0,0 +1,20 @@
+package br.com.cwi.crescer.lavanderia.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.cwi.crescer.dao.CidadeDAO;
+import br.com.cwi.crescer.domain.Cidade;
+
+public class CidadeDAOTest extends AbstractInfrastructureTest {
+
+    @Autowired
+    private CidadeDAO cidadeDAO;
+
+    @Test
+    public void deveBuscarCidadePorId() throws Exception {
+        Cidade cidade = cidadeDAO.findById(1L);
+        Assert.assertNotNull(cidade);
+    }
+}
