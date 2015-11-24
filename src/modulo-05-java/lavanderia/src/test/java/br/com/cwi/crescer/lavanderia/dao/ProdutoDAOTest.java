79f2e06c5e92fb750543f62a30e1758ef77a2e35
Tefferson <teffersonguterres@gmail.com>
2015-11-24 01:16:58 -0200

    refactor(Produto): mapear material e serviço.

--- src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDAOTest.java
+++ src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/lavanderia/dao/ProdutoDAOTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.cwi.crescer.dao.ProdutoDAO;
+import br.com.cwi.crescer.domain.Material;
 import br.com.cwi.crescer.domain.Produto;
+import br.com.cwi.crescer.domain.Servico;
 
 public class ProdutoDAOTest extends AbstractInfrastructureTest {
 
@@ -16,5 +18,17 @@ public class ProdutoDAOTest extends AbstractInfrastructureTest {
     public void deveBuscarProdutoPorId() throws Exception {
         Produto produto = produtoDAO.findById(1L);
         Assert.assertNotNull(produto);
+    }
+    
+    @Test
+    public void deveBuscarMaterial() throws Exception {
+        Material material = produtoDAO.findById(1L).getMaterial();
+        Assert.assertNotNull(material);
+    }
+    
+    @Test
+    public void deveBuscarServico() throws Exception {
+    	Servico servico = produtoDAO.findById(1L).getServico();
+        Assert.assertNotNull(servico);
     }
 }
