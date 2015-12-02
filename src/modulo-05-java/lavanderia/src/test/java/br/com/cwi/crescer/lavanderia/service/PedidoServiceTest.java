32e0d9dde38a647b53c80bfdfd9639aa0d6a5e26
Tefferson <teffersonguterres@gmail.com>
2015-12-01 22:24:06 -0200

    test(PedidoServiceTest): adicionar testes.

---
+++ src/modulo-05-java/lavanderia/src/test/java/br/com/cwi/crescer/lavanderia/service/PedidoServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.cwi.crescer.lavanderia.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PedidoServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
